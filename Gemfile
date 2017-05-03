6481d2e94431131246ad1b7db23c65fcfbd25102
Ryan Moore <moorer@udel.edu>
2017-05-03 16:23:09 -0400

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+# Specify your gem's dependencies in zeta_hunter.gemspec
+gemspec
---
+++ spec/spec_helper.rb
@@ -0,0 +1,29 @@
+# Copyright 2015 - 2017 Ryan Moore
+# Contact: moorer@udel.edu
+#
+# This file is part of ZetaHunter.
+#
+# ZetaHunter is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ZetaHunter is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with ZetaHunter.  If not, see <http://www.gnu.org/licenses/>.
+
+require "bundler/setup"
+require "zeta_hunter"
+
+RSpec.configure do |config|
+  # Enable flags like --only-failures and --next-failure
+  config.example_status_persistence_file_path = ".rspec_status"
+
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+end
